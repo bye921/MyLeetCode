579d2143c3986fd529d4e26f9bfcfabc3191128b
xu.zheng <xu.zheng@concentrix.com>
2019-09-20 10:27:46 +0800

    Add No.21, No.26, No.27

---
+++ src/main/java/com/trustno1/easy/no001to100/No0021.java
@@ -0,0 +1,74 @@
+package com.trustno1.easy.no001to100;
+
+/**
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ * 示例：
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+public class No0021 {
+
+    public static void main( String[] args ) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(4);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = null;
+
+        ListNode n4 = new ListNode(1);
+        ListNode n5 = new ListNode(3);
+        ListNode n6 = new ListNode(4);
+        n4.next = n5;
+        n5.next = n6;
+        n6.next = null;
+
+        ListNode l = mergeTwoLists(n1, n4);
+        while (l.next != null) {
+            System.out.print(l.val + " -> ");
+            l = l.next;
+        }
+        System.out.print(l.val);
+    }
+
+    private static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode temp = new ListNode(0);
+        ListNode newHead = temp;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                newHead.next = l1;
+                l1 = l1.next;
+            } else {
+                newHead.next = l2;
+                l2 = l2.next;
+            }
+            newHead = newHead.next;
+        }
+        newHead.next = l1 == null ? l2 : l1;
+        return temp.next;
+    }
+
+    private static ListNode mergeTwoListsRecursion(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoListsRecursion(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoListsRecursion(l1, l2.next);
+            return l2;
+        }
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
---
+++ src/main/java/com/trustno1/easy/no001to100/No0026.java
@@ -0,0 +1,41 @@
+package com.trustno1.easy.no001to100;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 示例 1:
+ * 给定数组 nums = [1,1,2],
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ * 你不需要考虑数组中超出新长度后面的元素。
+ *
+ * 示例 2:
+ * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ * 你不需要考虑数组中超出新长度后面的元素。
+ */
+public class No0026 {
+
+    public static void main( String[] args ) {
+        int[] numbers1 = new int[]{1,1,2};
+        int[] numbers2 = new int[]{0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println("input: " + Arrays.toString(numbers1) + ", Output length is " + removeDuplicates(numbers1));
+        System.out.println("input: " + Arrays.toString(numbers2) + ", Output length is " + removeDuplicates(numbers2));
+    }
+
+    private static int removeDuplicates(int[] numbers) {
+        if (numbers == null || numbers.length == 0) return 0;
+        if (numbers.length == 1) return 1;
+        int count = 0;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[count] != numbers[i]) {
+                count++;
+                numbers[count] = numbers[i];
+            }
+        }
+        return count + 1;
+    }
+}
